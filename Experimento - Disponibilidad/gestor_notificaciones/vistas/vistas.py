5cfd5f53c4734b5f8ad9a85c4651e5e6bbe65ff5
Julian Padilla Molina <julianpadilla@tyba.com.co>
2024-02-22 08:46:54 -0500

    Se realiza ajuste en el servicio get para comunicarse con la plataforma y enviar mensajes de error y exito

--- Experimento - Disponibilidad/gestor_notificaciones/vistas/vistas.py
+++ Experimento - Disponibilidad/gestor_notificaciones/vistas/vistas.py
@@ -1,3 +1,5 @@
+import random
+import requests
 from flask import request
 from ..modelos import db, Notificacion, NotificacionSchema
 from flask_restful import Resource
@@ -7,7 +9,15 @@ regla_schema = NotificacionSchema()
 class VistaNotificaciones(Resource):
 
     def get(self):
-        return [regla_schema.dump(regla) for regla in Notificacion.query.all()]
+        random_decimal_within_range = random.uniform(0, 9)
+        if random_decimal_within_range < 2:
+            json_payload = {'queue': 'monitor', 'message':  'El servicio de notificaciones no está disponible en este momento. Por favor, inténtelo más tarde.'}
+            requests.post('http:localhost:5000/send_message', data=json_payload, headers={'Content-Type': 'application/json'})
+            return [], 400
+        else:
+            json_payload = {'queue': 'monitor', 'message': 'El servicio de notificaciones ha sido consultado.'}
+            requests.post('http:localhost:5000/send_message', data=json_payload, headers={'Content-Type': 'application/json'})
+            return [regla_schema.dump(regla) for regla in Notificacion.query.all()], 200
 
     def post(self):
         nueva_notificacion = Notificacion(tipo_notificacion=request.json['tipo_notificacion'], titulo=request.json['titulo'], descripcion=request.json['descripcion'])
